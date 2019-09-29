f7dd79d71e24758955b8ffa9979cb44a3e27a547
Vladimir Zemskov <vlzemskovs@gmail.com>
2019-09-29 12:05:57 +0300

    fix

---
+++ ui/src/components/routeInfo.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const RouteInfoPage = props => {
+	return (
+		<ContentArea title="Категории">
+			<section className="dashboard-counts">
+
+			</section>
+		</ContentArea>
+	);
+};
