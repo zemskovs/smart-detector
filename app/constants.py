a507089a383e6710400e6ed01d8535bbbc93f6c0
Alexander Demidovskij <alexander.demidovskij@intel.com>
2019-10-06 17:10:01 +0300

    fixed typo in app config

--- app/constants.py
+++ app/constants.py
@@ -1,16 +1,14 @@
-import os
-
-ACCESS_TOKEN = 'ask Alexander about VK app'
+ACCESS_TOKEN = 'ask @demid5111 about VK app token'
 with open('.secret_access_token') as f:
     ACCESS_TOKEN = f.read()
 
-SENTIMENT_TOKEN = 'ask Alexander about sentiment service token'
+SENTIMENT_TOKEN = 'ask @demid5111 about sentiment service token'
 with open('.secret_sentiment_token') as f:
     SENTIMENT_TOKEN = f.read()
 
 DATA_FOLDER = '../db'
 
-EMAIL_PASSWORD = 'ask Alexander about sentiment service token'
+EMAIL_PASSWORD = 'ask @demid5111 about email password'
 with open('.secret_mail_password') as f:
     EMAIL_PASSWORD = f.read()
 
@@ -20,6 +18,7 @@ class Config:
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/test.db'.format(DATA_FOLDER)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
