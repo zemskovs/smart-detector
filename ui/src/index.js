516b37c1934600dcc7be0c164bb363b16adf7e27
Vladimir Zemskov <zemskov.va@esphere.ru>
2021-01-07 15:03:47 +0300

    правки страниц

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,17 +6,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { ExpertPage } from './pages/ExpertPage/ExpertPage';
 import { Feedback } from './pages/Feedback/FeedBack';
+import { RouteInfoPage } from './pages/RouteInfoPage/RouteInfoPage';
+import { CreateCategoryPage } from './pages/CreateCategoryPage/CreateCategoryPage';
 import { AllCategoryPage } from './components/AllCategoryPage';
 import { NewProposalPage } from './components/proposals/newProposalPage';
 import { ProposalRoutes } from './components/proposalRoutes';
 import { ProposalCard } from './components/proposalCard';
-import { CreateCategoryPage } from './components/createCategoryPage';
 import { onControlPage } from './components/proposals/onControl';
 import { onWorkPage } from './components/proposals/onWork';
 import { onOverduePage } from './components/proposals/onOverdue';
 import { AllProposalPage } from './components/proposals/allProposals';
-import { EditCategoryPage } from './components/editCategory';
-import { RouteInfoPage } from './components/routeInfo';
+import { EditCategoryPage } from './pages/EditCategoryPage/EditCategoryPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.default.css';
